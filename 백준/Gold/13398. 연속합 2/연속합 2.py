8366c4344a0d5486d036f5baaaed5af25c83d37f
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-02-14 21:10:42 +0900

    [Gold V] Title: 연속합 2, Time: 144 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -4,13 +4,13 @@ nums = list(map(int, input().split()))
 dp = [[0 for _ in range(N)] for _ in range(2)]
 
 dp[0][0] = nums[0]
+dp[1][0] = -1e9
+
 if N == 1 :
     print(nums[0])
 else :
-    ans = -1e9
     for i in range(1, N) :
         dp[0][i] = max(dp[0][i-1] + nums[i], nums[i])
         dp[1][i] = max(dp[1][i-1] + nums[i], dp[0][i-1])
-        ans = max(ans, dp[0][i], dp[1][i])
 
-    print(ans)
+    print(max(max(dp[0]), max(dp[1])))
