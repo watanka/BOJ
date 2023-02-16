77274152300b7c0050ceb441317a6b4bfed05c2b
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-02-16 22:34:40 +0900

    [Silver V] Title: 날짜 계산, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,16 @@
+## 날짜 계산
+E, S, M = map(int, input().split())
+
+N = 1
+
+e,s,m = 1,1,1
+while  not (e == E and s == S and m == M) :
+    e = N % 15 if N%15 else 15 
+    s = N % 28 if N%28 else 28
+    m = N % 19 if N%19 else 19
+    if e == E and s == S and m == M :
+        break
+
+    N += 1
+
+print(N)
