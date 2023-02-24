b21f33f8e3aef09abc23f4a7499986258e2e552c
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-02-24 15:32:40 +0900

    [Silver I] Title: 링크와 스타트, Time: 2092 ms, Memory: 130540 KB -BaekjoonHub

--- 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
+++ 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
@@ -2,31 +2,33 @@
 N = int(input())
 S = [list(map(int, input().split())) for _ in range(N)]
 
+num_players = N//2
 min_diff = 100*20
 
-visited = [0] * N
-def score() :
+
+def dfs(L, start, cnt) :
     global min_diff
-        # print(visited)
-    stat1, stat2 = 0, 0
-    for i in range(N-1) :
-        for j in range(i+1, N) :
-            if visited[i] and visited[j] :
-                stat1 += S[i][j] + S[j][i]
-            elif visited[i] == 0 and visited[j] == 0 :
-                stat2 += S[i][j] + S[j][i]
-    min_diff = min(min_diff, abs(stat1 - stat2))
-    
-def recur(target) :
-    if target == N :
-        score()
+    if cnt == L :
+        stat1, stat2 = 0, 0
+        for i in range(N-1) :
+            for j in range(i+1,N) :
+                if visited[i] and visited[j] :
+                    stat1 += S[i][j] + S[j][i]
+                elif visited[i] == 0 and visited[j] == 0 :
+                    stat2 += S[i][j] + S[j][i]
+        min_diff = min(min_diff, abs(stat1 - stat2))
         return
+    
+    for i in range(start, N) :
+        if not visited[i] :
+            visited[i] = 1
+            dfs(L, i+1, cnt+1)
+            visited[i] = 0
 
-    visited[target] = 1
-    recur(target + 1)
-    visited[target] = 0
-    recur(target + 1)
 
+for num_player in range(1, num_players + 1) :
+    visited = [0] * N
+    dfs(num_player, 0, 0)
 
-recur(0)
 print(min_diff)
+
