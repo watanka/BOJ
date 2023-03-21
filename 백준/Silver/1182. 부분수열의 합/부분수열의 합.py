9f62ad31c870c7dd019972653ab0a2c1dcadbcfc
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-03-21 14:38:17 +0900

    [Silver II] Title: 부분수열의 합, Time: 264 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,22 +1,20 @@
 ## 부분수열의 합
+
 N, S = map(int, input().split())
-nums = list(map(int, input().split()))
-
+num_list = list(map(int, input().split()))
 cnt = 0
-stack = []
-
-def subset_sum(idx, total) :
+def partsum(idx, total) :
     global cnt
     if idx == N :
         return
-    
-    total += nums[idx] 
+
+    total += num_list[idx]
     if total == S :
         cnt += 1
 
-    subset_sum(idx+1, total)
-    subset_sum(idx+1, total - nums[idx])
+    partsum(idx + 1, total )
+    partsum(idx + 1, total - num_list[idx])
 
-subset_sum(0, 0)
+partsum(0, 0)
 
 print(cnt)
