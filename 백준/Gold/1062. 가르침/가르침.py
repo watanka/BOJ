95bc84c4de8f1a5b71126e287222446cdec59bd9
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-05-16 15:57:29 +0900

    [Gold IV] Title: 가르침, Time: 1516 ms, Memory: 117488 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -0,0 +1,47 @@
+import sys
+
+n, k = map(int, input().split())
+
+# k 가 5보다 작으면 어떤 언어도 배울 수 없음
+if k < 5:
+    print(0)
+    exit()
+# k 가 26이면 모든 언어를 배울 수 있음
+elif k == 26:
+    print(n)
+    exit()
+
+answer = 0
+words = [set(sys.stdin.readline().rstrip()) for _ in range(n)]
+learn = [0] * 26
+
+# 적어도 언어 하나는 배우기위해 a,c,i,n,t 는 무조건 배워야함
+for c in ('a', 'c', 'i', 'n', 't'):
+    learn[ord(c) - ord('a')] = 1
+
+
+def dfs(idx, cnt):
+    global answer
+
+    if cnt == k - 5:
+        readcnt = 0
+        for word in words:
+            check = True
+            for w in word:
+                if not learn[ord(w) - ord('a')]:
+                    check = False
+                    break
+            if check:
+                readcnt += 1
+        answer = max(answer, readcnt)
+        return
+
+    for i in range(idx, 26):
+        if not learn[i]:
+            learn[i] = True
+            dfs(i, cnt + 1)
+            learn[i] = False
+
+
+dfs(0, 0)
+print(answer)
