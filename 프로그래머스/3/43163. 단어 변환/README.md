211e5ea0ec41f11f53bd27d1d8926382e2a56c76
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-11-03 17:00:34 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163#) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 16:37:37
+2023년 11월 5일 17:0:32
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,52 +1,34 @@
 from collections import deque
 
+
+def get_adjacent(current, words):
+    for word in words:
+        if len(current) != len(word):
+            continue
+
+        count = 0
+        for c, w in zip(current, word):
+            if c != w:
+                count += 1
+
+        if count == 1:
+            yield word
+
+
 def solution(begin, target, words):
+    dist = {begin: 0}
+    queue = deque([begin])
+
+    while queue:
+        current = queue.popleft()
+
+        for next_word in get_adjacent(current, words):
+            if next_word not in dist:
+                dist[next_word] = dist[current] + 1
+                queue.append(next_word)
+
+    return dist.get(target, 0)
     
-    def diff_by_one(word1, word2) :
-        cnt = 0
-        for c1, c2 in zip(word1, word2) :
-            if c1 != c2 :
-                cnt += 1
-                if cnt > 1 :
-                    return False
-        return True
-    
-    visited = [0 for _ in range(len(words))]
-    found = False
-    
-    trg_idx = 0
-    for i, word in enumerate(words) :
-        if word == target :
-            found = True
-            trg_idx = i
-            visited[i] = 1
             
-    if not found :
-        return 0
-    
-    
-    def bfs() :
-        nonlocal trg_idx
-        queue = deque([(target, trg_idx)])
-        
-        while queue :
-            trg, trg_idx = queue.popleft()
+                
             
-            for i, word in enumerate(words) :
-                if visited[i] == 0 and diff_by_one(word, trg) :
-                    visited[i] = visited[trg_idx] + 1
-                    queue.append([word, i])
-                    
-            
-        
-    bfs()
-    answer = 51
-    found = False
-    for i, word in enumerate(words) :
-        if diff_by_one(word, begin) :
-            answer = min(answer, visited[i])
-            found = True
-            
-    return answer if found else 0
-    
-        
