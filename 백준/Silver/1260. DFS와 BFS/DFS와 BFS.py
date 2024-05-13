e476b8d9b9174ce1a349db3a891640fee164336b
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-05-13 22:08:06 +0900

    [Silver II] Title: DFS와 BFS, Time: 656 ms, Memory: 36700 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,41 @@
-## DFS와 BFS
+from collections import deque
 
-N, M, V = map(int, input().split())
-net = [[] for _ in range(N+1)]
-for _ in range(M) :
-    a, b = map(int, input().split())
-    net[a].append(b)
-    net[b].append(a)
+N, M, V = map(int, input().split(' '))
 
-for i in range(len(net)) :
-    net[i].sort()
+relations = [map(int, input().split(' ')) for _ in range(M)]
 
+graph = [[] for _ in range(N+1)]
+for start, end in relations:
+    graph[start].append(end)
+    graph[end].append(start)
 
-def dfs(start_node) :
-    stack = []
+for i in range(len(graph)):
+    graph[i].sort()
+
+# DFS
+def dfs(start_node):
     visited = []
+    stack = []
     stack.append(start_node)
-
-    while stack :
+    while stack:
         node = stack.pop()
-        if node not in visited :
+        if node not in visited:
             visited.append(node)
-            stack.extend(net[node][::-1])
+            stack.extend(graph[node][::-1])
 
     return visited
 
 
+# BFS
 
-def bfs(start_node) :
-    queue = []
+def bfs(start_node):
     visited = []
-    queue.append(start_node)
-    while queue :
-        node = queue.pop(0)
-        if node not in visited :
+    q = deque([start_node])
+    while q:
+        node = q.popleft()
+        if node not in visited:
             visited.append(node)
-            queue.extend(net[node])
-
+            q.extend(graph[node])
     return visited
 
 print(*dfs(V))
